99161366ed477e71753f02c94ada4c5c2d232b8c
wwapx <hemersonm@icloud.com>
2023-05-05 08:42:18 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+three.js/sea3d
+========
+
+The aim of the project is to create an easy to use custom editor to work with the sea3d model format.
+
+# Features
+* Import .SEA models.
+* Batch Export Textures.
+* Export to any supported format.
+
+# Supported formats
+* Collada (.DAE) - Highly Recommended
+* GLTF
+* OBJ
+* PLY
+* STL
+* Draco
+
+# Dependencies
+* [Three.js rev 1.1.1](https://github.com/mrdoob/three.js/releases/tag/r111)
+* [SEA3D 1.8](https://github.com/sunag/sea3d)
---
+++ editor/js/Menubar.Help.js
@@ -0,0 +1,60 @@
+/**
+ * @author mrdoob / http://mrdoob.com/
+ */
+
+Menubar.Help = function ( editor ) {
+
+	var strings = editor.strings;
+
+	var container = new UI.Panel();
+	container.setClass( 'menu' );
+
+	var title = new UI.Panel();
+	title.setClass( 'title' );
+	title.setTextContent( strings.getKey( 'menubar/help' ) );
+	container.add( title );
+
+	var options = new UI.Panel();
+	options.setClass( 'options' );
+	container.add( options );
+
+	// Source code
+
+	var option = new UI.Row();
+	option.setClass( 'option' );
+	option.setTextContent( strings.getKey( 'menubar/help/source_code' ) );
+	option.onClick( function () {
+
+		window.open( 'https://github.com/mrdoob/three.js/tree/master/editor', '_blank' )
+
+	} );
+	options.add( option );
+
+
+	// SEA3D
+
+	var option = new UI.Row();
+	option.setClass( 'option' );
+	option.setTextContent( 'SEA3D v1.8' );
+	option.onClick( function () {
+
+		window.open( 'https://github.com/sunag/sea3d', '_blank' );
+
+	} );
+	options.add( option );
+
+	// About
+
+	var option = new UI.Row();
+	option.setClass( 'option' );
+	option.setTextContent( strings.getKey( 'menubar/help/about' ) );
+	option.onClick( function () {
+
+		window.open( 'http://threejs.org', '_blank' );
+
+	} );
+	options.add( option );
+
+	return container;
+
+};
